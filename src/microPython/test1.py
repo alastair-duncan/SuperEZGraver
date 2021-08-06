a274b8035c0e40d4368231214affac416d8f7c71
ps-bond <ps-bond@users.noreply.github.com>
2021-08-06 21:29:14 +0100

    More handpiece mods

--- src/microPython/test1.py
+++ src/microPython/test1.py
@@ -2,25 +2,30 @@
 # First test using microPython
 #
 
-import machine, time
+import machine
+import time
 from machine import Pin, PWM, ADC
 
 pulseLength = 10     # ms
 maxFreq = 5000/60   # Hz - representation is Strokes Per Minute
-maxADCdeflection = 1024
+maxADCdeflection = 1024  # depends on the board
 outputPin = 0
 speedPin = 1
 powerPin = 2
-speedBias = 20 # ignore any readings below this and don't switch on
-pwmFreq = 1000 # max 1000
+speedBias = 20  # ignore any readings below this and don't switch on
+pwmFreq = 1000  # max 1000
 # configure PWM output pin
 #  Duty cycle is 0..1023
 pwmPin = PWM(Pin(outputPin))
 pwmPin.freq(pwmFreq)
 
 # configure input pin for analogue input - speed
-# NB needs to be 0..1.0VDC
+# NB needs to be 0..1.0VDC?
 # read() method returns 0..1024 - check this, really not 1023?
+# ESP32 docs claim 0..4095; attenuation available; 12 bit resolution
+# ESP8266 says 0..1024, no attenuation mentioned; 10 bit resolution
+# Pi Pico 0..4095; 12 bit resolution, 0..3V3
+
 speedSetting = ADC(speedPin)
 
 # configure input pin for analogue input - PWM duty cycle (power)
@@ -29,13 +34,14 @@ powerSetting = ADC(powerPin)
 # loop
 while (True):
 
-    # Read speed analogue input, quantise 
-    currentSpeed = maxADCdeflection - speedSetting.read() # Low resistance = high Voltage, high speed
-    # Read power input 
-    currentPower = maxADCdeflection - powerSetting.read() # Low resistance = high power
+    # Read speed analogue input, quantise
+    # Low resistance = high Voltage, high speed
+    currentSpeed = maxADCdeflection - speedSetting.read()
+    # Read power input
+    currentPower = maxADCdeflection - powerSetting.read()  # Low resistance = high power
 
     if (currentSpeed >= speedBias):
-        # set PWM output level on   
+        # set PWM output level on
         pwmPin.duty(currentPower)
         # Pulse length dictates duty cycle, determine minimum possible.
         # For max speed of 5000SPM can't go above 6ms and manage 50% duty cycle (assuming max power)
@@ -43,10 +49,8 @@ while (True):
         # Set output low - off time
         pwmPin.duty(0)
 
-        quantisedPeriod = 1/(maxFreq * currentSpeed/maxADCdeflection) * 1000 # ms
+        # ms  Note that without the bias setting we're into divide-by-zero territory
+        quantisedPeriod = 1/(maxFreq * currentSpeed/maxADCdeflection) * 1000
         time.sleep_ms(quantisedPeriod - pulseLength)
     else:
-        time.sleep_ms(100) #  Because why not
-
-
-
+        time.sleep_ms(100)  # Because why not
