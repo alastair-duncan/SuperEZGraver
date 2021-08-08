3783c8c7497a887edf69e82f60c564d1defc88b5
ps-bond <ps-bond@users.noreply.github.com>
2021-08-08 11:30:10 +0100

    Cmd line run test

---
+++ src/microPython/cmdLineTest.py
@@ -0,0 +1,38 @@
+# Command line testing
+import machine
+import time
+from machine import Pin, PWM
+
+outputPin = 0
+pulseLength = 10     # ms
+maxFreq = 5000/60   # Hz - representation is Strokes Per Minute
+speedBias = 0.05  # ignore any readings below this and don't switch on
+pwmFreq = 1000  # max 1000
+pwmRange = 1023
+
+def runGraver(speed, power, duration):
+    # speed 0.0..1.0
+    # power 0.0..1.0
+    # duration seconds
+    offTime = 1000/(maxFreq * speed) - pulseLength
+    assert True, offTime >= pulseLength # 50% overall duty cycle max
+
+    pwmPin = PWM(Pin(outputPin))
+    pwmPin.freq(pwmFreq)
+
+    if speed >= speedBias:
+        startTime = time.ticks_ms()
+        while time.ticks_diff(time.ticks_ms(), startTime) < duration * 1000:
+                # on
+                pwmPin.duty(power * pwmRange)
+                time.sleep_ms(pulseLength)
+                # Switch off
+                pwmPin.duty(0)
+                # Wait
+                time.sleep_ms(offTime)
+        print("Duration complete")
+
+    else:
+        print("Speed < bias, ignore")
+
+    pwmPin.deinit()
--- src/microPython/graver.py
+++ src/microPython/graver.py
@@ -9,10 +9,10 @@ class Graver(threading.Thread):
     __debug = True
 
     def __init__(self, speedQueue, powerQueue):
-        self.currentSpeed = 0
-        self.currentPower = 0
-        self.pulseLength = 10     # ms
-        self.maxFreq = 2500/60   # Hz - representation is Strokes Per Minute
+        self.currentSpeed = 0.0     # 0..1.0
+        self.currentPower = 0.0     # 0..1.0
+        self.pulseLength = 10       # ms
+        self.maxFreq = 2500/60      # Hz - representation is Strokes Per Minute
         self.bias = 0.05
 
         self.speedQueue = speedQueue
@@ -20,18 +20,18 @@ class Graver(threading.Thread):
 
         self.__running = True
 
-    def __readQueues():
+    def __readQueues(self):
         # Speed
         # lock
         if not self.speedQueue.empty():
             self.currentSpeed = self.speedQueue.get()
-            if __debug:
-                print("graver - speed read as ", self.currentSpeed)
+            if self.__debug:
+                print("graver - speed read as ", (int)(self.currentSpeed * 100), "%")
 
         if not self.powerQueue.empty():
             self.currentPower = self.powerQueue.get()
-            if __debug:
-                print("graver - power read as ", self.currentPower)
+            if self.__debug:
+                print("graver - power read as ", (int)(self.currentPower * 100), "%")
 
     def setSpeed(self, speed):
         self.currentSpeed = speed
@@ -54,12 +54,23 @@ class Graver(threading.Thread):
             if self.CurrentSpeed > self.bias:
                 # Switch on
                 # Wait
+                time.sleep_ms(self.pulseLength)
                 # Switch off
                 # Wait
-            else
+                offTime = 1000/(self.maxFreq * self.currentSpeed) - self.pulseLength
+                time.sleep_ms(offTime)
+
+                if self.__debug:
+                    print("Tick")
+            else:
                 # sleep 100ms
+                time.sleep_ms(50)   # 1/2 of sample rate for input
+                if self.__debug:
+                    print("Speed < bias")
 
         # Cleanup - shutdown output
+        if self.__debug:
+            print("Shutdown")
 
 # Check limitations of microPython + threads
 
@@ -69,25 +80,28 @@ class Input(threading.Thread):
 
     __debug = True
 
-    def __init__(self, pin, range, outputQueue)
+    def __init__(self, pin, range, outputQueue):
         # Configure the pin for ADC
         self.pin = pin
         self.range = range
         self.outputQueue = outputQueue
-
-        self.sleepTime = 100 # ms
+        self.currentValue = 0.0
+        self.sleepTime = 100 # ms - sample rate
 
         self.__running = True
 
-    def terminate():
+    def terminate(self):
         self.__running = False
 
     def run(self):
         while self.__running:
             # Read input
+            self.currentValue = 0.0
             # Get queue lock
             # Push value to queue (NB do we interpret this to a fixed range? 0..1 for example?)
             # Release queue lock
             # sleep
+            if self.__debug:
+                print("Current input reading for ", self, " = ", self.currentValue)
 
         # Cleanup
